fb88bd6edfc9a139c5adfa1d52ceff22403c80b6
Stephen Scaff <stephen.scaff@sosweetcreative.com>
2015-01-31 06:48:10 -0800

    Improvements

--- kit/assets/js/plugins/_page-transitions.js
+++ kit/assets/js/plugins/_page-transitions.js
@@ -1,34 +1,16 @@
 /*----------------------------------------
 Page Transitions
 ------------------------------------------*/
+$("body").css("display", "none");
 
-//Fade in page all sexy like
-$('body').addClass('kf-fade-in-2');
+$("body").fadeIn(1500);
 
-//Dump the fade-in class - prevent weird webkit shit  
-setTimeout(function(){
-$("body").removeClass("kf-fade-in-2");
-},2100);
+$("a.suave, a.btn, nav a, .sect-blocks a").click(function(event){
+	event.preventDefault();
+	linkLocation = this.href;
+	$("body").fadeOut(600, redirectPage);
+});
 
-
-//PAGE TRANSITIONS - Leaving page fades
-$.fn.leavePage = function() {   
-this.click(function(event){
-
-// Hold up on heading to the next page.
-event.preventDefault();
-linkLocation = this.href;
-
-// Fade out first - all sexy like.
-$('header').animate({'opacity':'0', 'top':'-92px'}, 500, 'easeOutExpo');
-$('body').animate('slow').fadeOut(500, function(){
-
-// Alighty - now head over to new page.
-window.location = linkLocation;
-$('header').stop().animate({'opacity': '1', 'top':'0'}, 1000);
-});      
-}); 
-};
-//apply the leave page transition to certian links
-$('a.js-trans').leavePage();
-
+function redirectPage() {
+	window.location = linkLocation;
+}
--- kit/assets/js/scripts.js
+++ kit/assets/js/scripts.js
@@ -23,9 +23,43 @@ $("#trigger-nav, .closed, .siteoverlay").click(function (e) {
   });
 
 /*----------------------------------------------   
--Script Details
------------------------------------------------  */ 
-
+-Simple Scroll To Anchor
+-----------------------------------------------  */
+    $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      var target = "#js-scrolly",
+        $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 70
+      }, 600, 'swing');
+    });
+/*----------------------------------------------   
+-Disable hovers on scroll
+-----------------------------------------------  */
+    var body = document.body,
+      timer;
+    window.addEventListener('scroll', function() {
+      clearTimeout(timer);
+      if (!body.classList.contains('disable-hover')) {
+        body.classList.add('disable-hover')
+      }
+      timer = setTimeout(function() {
+        body.classList.remove('disable-hover')
+      }, 100);
+    }, false);
+    var eT = 0;
+    $('.sect-blocks article').hide().each(function() {
+      $(this).delay(eT).fadeIn(1000);
+      eT += 350;
+    });
+    
+ /*----------------------------------------------   
+ -Color Overlays on Hover
+ -----------------------------------------------  */
+     $(".home a.alpha").hover(function() {
+       $(".overlay").toggleClass("overlay-alpha");
+     }); 
+    
 });
 }).call(this);
 
